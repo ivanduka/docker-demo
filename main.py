4767cdd10254757688cf441a81bcfc6537e53787
Ivan Duka <39438519+ivanduka@users.noreply.github.com>
2021-12-21 16:11:59 -0700

    updates

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import uuid
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from dotenv import load_dotenv
+import glob
 
 # Load environmental variables from .env file if present
 load_dotenv()
@@ -14,21 +15,33 @@ load_dotenv()
 ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == "__main__":
-    azure_key = os.getenv('AZURE_KEY')
-    if azure_key == None:
-        raise Exception('Must have AZURE_KEY environmental variable to work')
+    print("Working...")
 
+    azure_conn_str = os.getenv('AZURE_CONNECTION_STRING')
+    if azure_conn_str == None:
+        raise Exception(
+            'Must have AZURE_CONNECTION_STRING environmental variable to work')
 
     id = os.getenv('FILE_ID')
     if id == None:
         raise Exception('Must have FILE_ID environmental variable to work')
 
-    
     url = f'https://apps.cer-rec.gc.ca/REGDOCS/File/Download/{id}'
     tables = camelot.read_pdf(url)
     tables.export('foo.csv', f='csv')
 
-    
+    blob_service_client = BlobServiceClient.from_connection_string(
+        azure_conn_str)
 
-    print(os.listdir())
+    container_name = str(uuid.uuid4())
+    blob_service_client.create_container(container_name)
+    print(f"Container name: {container_name}")
+
+    files = glob.glob('*.csv')
+    for f in files:
+        print(f"Uploading {f}")
+        with open(f, "rb") as data:
+          blob_client = blob_service_client.get_blob_client(container=container_name, blob=os.path.basename(f))
+          blob_client.upload_blob(data)
+
     print("Done.")
