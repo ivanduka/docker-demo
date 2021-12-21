19fb1d83420623da0866b5606a7e28839ef1c1e1
Ivan Duka <39438519+ivanduka@users.noreply.github.com>
2021-12-21 15:01:07 -0700

    updates

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from ctypes.util import find_library
 import camelot
 import os
+import ssl
+
+# Fix bug on MacOS
+ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == "__main__":
     id = os.getenv('FILE_ID')
     if id == None:
-      raise Exception('Must have FILE_ID environmental variable to work')
+        raise Exception('Must have FILE_ID environmental variable to work')
+        
     url = f'https://apps.cer-rec.gc.ca/REGDOCS/File/Download/{id}'
     tables = camelot.read_pdf(url)
     tables.export('foo.csv', f='csv')
